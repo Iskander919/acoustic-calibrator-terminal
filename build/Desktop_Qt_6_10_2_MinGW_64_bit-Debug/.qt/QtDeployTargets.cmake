0357c08b769b5a02aa7cf6bd989d64c4f82cd31f
Iskander919 <iskan.sitdikov@yandex.ru>
2026-02-06 17:07:47 +0300

    Delete build directory

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_acoustic-calibrator-terminal_FILE C:/Users/Iskander/Documents/NTM/acoustic-calibrator-terminal/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/acoustic-calibrator-terminal.exe)
-set(__QT_DEPLOY_TARGET_acoustic-calibrator-terminal_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_acoustic-calibrator-terminal_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6SerialPort.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
