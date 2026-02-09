c03b63648b12bb6017ef08fc2878be5fde3dfcec
Iskander919 <iskan.sitdikov@yandex.ru>
2026-02-09 21:13:46 +0300

    Implement parser

--- com_port_driver/com_port_driver.cpp
+++ com_port_driver/com_port_driver.cpp
@@ -1,11 +1,18 @@
 #include "com_port_driver.h"
 
+/**
+ * @brief SerialDriver::SerialDriver
+ */
 SerialDriver::SerialDriver() {
 
     this -> SerialPortObj = new QSerialPort();
 
 }
 
+/**
+ * @brief SerialDriver::openSerialPort
+ * @param portName
+ */
 void SerialDriver::openSerialPort(const QString &portName) {
 
     this -> SerialPortObj -> setPortName(portName);
@@ -22,5 +29,64 @@ void SerialDriver::openSerialPort(const QString &portName) {
         qDebug() << "COM Port Error: " << this -> SerialPortObj -> error();
 
     }
+
+}
+
+/**
+ * @brief SerialDriver::combineArray
+ * @param pCoeff
+ * @param iCoeff
+ * @param dCoeff
+ * @param corr
+ * @param ref
+ * @return
+ */
+std::array<uint8_t, 20> SerialDriver::combineArray(const float pCoeff, const float iCoeff, const float dCoeff,
+                                     const float corr, const uint32_t ref) {
+
+    std::array<uint8_t, 20> result;
+
+    // getting bytes from pCoeff float and storing them to array
+    std::array<uint8_t, 4> pBytes = std::bit_cast<std::array<uint8_t, 4>>(pCoeff);
+
+    // getting bytes from iCoeff
+    std::array<uint8_t, 4> iBytes = std::bit_cast<std::array<uint8_t, 4>>(iCoeff);
+
+    // getting bytes from dCoeff
+    std::array<uint8_t, 4> dBytes = std::bit_cast<std::array<uint8_t, 4>>(dCoeff);
+
+    // getting bytes from corr
+    std::array<uint8_t, 4> corrBytes = std::bit_cast<std::array<uint8_t, 4>>(corr);
+
+    // getting bytes from ref
+    std::array<uint8_t, 4> refBytes  = std::bit_cast<std::array<uint8_t, 4>>(ref);
+
+    // filling result:
+    for(int i = 0; i < 4; i++) {
+
+        result[i] = pBytes[i];
+    }
+
+    for(int i = 0; i < 4; i++) {
+
+        result[i + 4] = iBytes[i];
+    }
+
+    for(int i = 0; i < 4; i++) {
+
+        result[i + 8] = dBytes[i];
+    }
+
+    for(int i = 0; i < 4; i++) {
+
+        result[i + 12] = corrBytes[i];
+    }
+
+    for(int i = 0; i < 4; i++) {
+
+        result[i + 16] = refBytes[i];
+    }
+
+    return result;
 
 }
--- com_port_driver/com_port_driver.h
+++ com_port_driver/com_port_driver.h
@@ -6,6 +6,8 @@
 #include <QSerialPortInfo>
 #include <QIODevice>
 #include <QList>
+#include <array>
+#include <bit>
 
 #define BAUD_RATE QSerialPort::BaudRate::Baud115200
 
@@ -17,9 +19,18 @@ public:
 
     void openSerialPort(const QString &portName);
 
+    void sendCommand(const uint8_t data[20], const uint8_t command);
+
+    std::array<uint8_t, 20> combineArray(const float pCoeff, const float iCoeff, const float dCoeff,
+                                                                                  const float corr,
+                                                                                  const uint32_t ref);
+
 private:
 
     QSerialPort *SerialPortObj;
+
+    std::array<uint8_t, 20> data;
+    uint8_t command;
 
 };
 
